de6a5aa2af69022ac0433bde5bc28c4469310026
Calstins <opulecalebtins@gmail.com>
2023-11-11 05:19:56 -0800

    updated listing model

--- api/models/listingModel.js
+++ api/models/listingModel.js
@@ -10,7 +10,19 @@ const listingSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    address: {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
       type: String,
       required: true,
     },
@@ -40,11 +52,58 @@ const listingSchema = new mongoose.Schema(
     },
     category: {
       type: String,
+      enum: ['rent', 'sale', 'shortlet', 'jventure'],
       required: true,
     },
     type: {
       type: String,
+      enum: ['flat', 'house', 'land', 'commercial', 'venue'],
       required: true,
+    },
+    subtype: {
+      type: String,
+      required: function () {
+        return ['flat', 'house', 'land', 'commercial', 'venue'].includes(
+          this.type
+        );
+      },
+      enum: function () {
+        switch (this.type) {
+          case 'flat':
+            return ['mini', 'self-contain'];
+          case 'house':
+            return [
+              'd-bungalow',
+              'd-duplex',
+              's-bungalow',
+              's-duplex',
+              't-bungalow',
+              't-duplex',
+            ];
+          case 'land':
+            return ['residential', 'commercial', 'mixed-used', 'industrial'];
+          case 'house':
+            return [
+              'church',
+              'factory',
+              'filling-station',
+              'hotel',
+              'office',
+              'plaza',
+              'restaurant',
+              'shop',
+              'school',
+              'tank-farm',
+              'warehouse',
+            ];
+          case 'commercial':
+            return ['Studio', '1 Bedroom', '2 Bedrooms', '3+ Bedrooms'];
+          case 'venue':
+            return ['conference', 'hall'];
+          default:
+            return [];
+        }
+      },
     },
     offer: {
       type: Boolean,
