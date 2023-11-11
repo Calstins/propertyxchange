6eb2c9c7e5f7da58ac414afbfb34555b9681312a
Calstins <opulecalebtins@gmail.com>
2023-11-11 06:52:32 -0800

    updated listing model 2

--- api/models/listingModel.js
+++ api/models/listingModel.js
@@ -33,6 +33,27 @@ const listingSchema = new mongoose.Schema(
     discountPrice: {
       type: Number,
       required: true,
+      default: 0,
+    },
+    appendTo: {
+      type: String,
+      required: false,
+    },
+    initialPayment: {
+      type: Number,
+      required: false,
+    },
+    monthlyPayment: {
+      type: Number,
+      required: false,
+    },
+    duration: {
+      type: Number,
+      required: false,
+    },
+    features: {
+      type: [String],
+      required: false,
     },
     bathrooms: {
       type: Number,
@@ -42,7 +63,19 @@ const listingSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    toilets: {
+      type: Number,
+      required: true,
+    },
     furnished: {
+      type: Boolean,
+      required: true,
+    },
+    serviced: {
+      type: Boolean,
+      required: true,
+    },
+    newlyBuilt: {
       type: Boolean,
       required: true,
     },
@@ -50,60 +83,25 @@ const listingSchema = new mongoose.Schema(
       type: Boolean,
       required: true,
     },
+    youtubeLink: {
+      type: String,
+      required: false,
+    },
+    instagramLink: {
+      type: String,
+      required: false,
+    },
     category: {
       type: String,
-      enum: ['rent', 'sale', 'shortlet', 'jventure'],
       required: true,
     },
     type: {
       type: String,
-      enum: ['flat', 'house', 'land', 'commercial', 'venue'],
       required: true,
     },
     subtype: {
       type: String,
-      required: function () {
-        return ['flat', 'house', 'land', 'commercial', 'venue'].includes(
-          this.type
-        );
-      },
-      enum: function () {
-        switch (this.type) {
-          case 'flat':
-            return ['mini', 'self-contain'];
-          case 'house':
-            return [
-              'd-bungalow',
-              'd-duplex',
-              's-bungalow',
-              's-duplex',
-              't-bungalow',
-              't-duplex',
-            ];
-          case 'land':
-            return ['residential', 'commercial', 'mixed-used', 'industrial'];
-          case 'house':
-            return [
-              'church',
-              'factory',
-              'filling-station',
-              'hotel',
-              'office',
-              'plaza',
-              'restaurant',
-              'shop',
-              'school',
-              'tank-farm',
-              'warehouse',
-            ];
-          case 'commercial':
-            return ['Studio', '1 Bedroom', '2 Bedrooms', '3+ Bedrooms'];
-          case 'venue':
-            return ['conference', 'hall'];
-          default:
-            return [];
-        }
-      },
+      required: false,
     },
     offer: {
       type: Boolean,
